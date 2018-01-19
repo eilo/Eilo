a1c49a85f41a3d10036e684962011509c01668fc
Eilo <eilo2518@gmail.com>
2018-01-19 02:50:47 -0500

    0.77 RaidEspectador: Añadiendo el sql de comandos para world, va a estar medio dificil cumplir lo que pongo en las descripciones xD

---
+++ sql/rean/world/2018_01_19_100_world_raid_espectador_comandos.sql
@@ -0,0 +1,19 @@
+-- Eilo (https://github.com/eilo)
+-- Raid Espectador WowRean
+-- Definición de los comandos
+
+DELETE FROM `command` WHERE `name`='raidespectador';
+INSERT INTO `command` (`name`, `permission`, `help`) VALUES
+('raidespectador', 1001, 'Sintaxis: .raidespectador\nDesplega las opciones del comando.');
+
+DELETE FROM `command` WHERE `name`='raidespectador lista';
+INSERT INTO `command` (`name`, `permission`, `help`) VALUES
+('raidespectador lista', 1002, 'Sintaxis: .raidespectador lista\nDevuelve la lista de raids, líder y guild(de ser el caso) que actualmente estan armadas y dentro de una instance en el servidor.');
+
+DELETE FROM `command` WHERE `name`='raidespectador entrar';
+INSERT INTO `command` (`name`, `permission`, `help`) VALUES
+('raidespectador entrar', 1003, 'Sintaxis: .raidespectador entrar idraid\nTe teletransporta en modo espectador al id de raid seleccionado.');
+
+DELETE FROM `command` WHERE `name`='raidespectador salir';
+INSERT INTO `command` (`name`, `permission`, `help`) VALUES
+('raidespectador salir', 1004, 'Sintaxis: .raidespectador salir\nTe regresa al lugar antes de teletransportarte, dejando dejando de espectar el raid.');
